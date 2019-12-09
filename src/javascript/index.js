e41497bd83d8a78415c2ff9ce50e7e5cff17fb31
Pepijn Friederichs <pepijn@chello.nl>
2019-12-09 19:19:53 +0100

    src files

---
+++ src/javascript/index.js
@@ -0,0 +1,9 @@
+
+import {init} from "./progress-bar";
+
+const elements = document.querySelectorAll('.js-progress-elements li');
+
+init(elements);
+
+
+
---
+++ src/javascript/progress-bar.js
@@ -0,0 +1,75 @@
+/*
+ this file contains de ProgressBar html5 element <progress> global object which is created on the page when needed
+ this object is written in vanillaJS
+ arrayOfElements must be javascript array
+
+ more on this element and browser compatibility : https://developer.mozilla.org/en-US/docs/Web/HTML/Element/progress
+ */
+
+/* exported ProgressBar */
+
+let elements;
+let countProgress = 0;
+let length;
+let bar;
+let parts;
+
+
+const bindElementsToProgress = (elm) => {
+
+    elm.addEventListener('click', (e) =>{
+        //console.log(e.target, i);
+        let target =  e.target;
+        if (target.classList.contains('progress--invoker') && !target.classList.contains('progress--invoked')){
+            target.classList.add('progress--invoked');
+            countProgress++;
+            setValue(parts);
+            let nextElement = elm.nextElementSibling;
+            if (nextElement !== null) nextElement.querySelector('.progress--invoker').classList.remove('link--disabled');
+        }
+
+
+    });
+};
+
+const getNodeList = (arrayOfElements) => {
+
+    elements = Array.from(arrayOfElements); // convert nodeList to Array
+    length = elements.length;
+    elements.forEach(bindElementsToProgress);
+
+};
+
+const createProgressBar = () => {
+    bar = document.createElement('progress');
+    bar.className = 'progress js-progress-bar';
+    bar.id = 'progress-bar';
+    bar.setAttribute('max', 100);
+    bar.value = 0;
+    document.body.insertBefore(bar, document.body.firstChild);
+};
+
+const setValue = (percentage) => {
+    const value = percentage * countProgress; //this is the value which increases the progress bar
+    bar.setAttribute('value', value);
+};
+
+const setProgressBarParts = (length) => {
+
+    const maxValue = bar.getAttribute('max');
+    return maxValue / length;
+
+};
+
+export const init = (arrayOfElements) => {
+
+    if (!document.querySelector('.js-progress-bar')) {
+        getNodeList(arrayOfElements);
+        createProgressBar();
+        setTimeout(parts = setProgressBarParts(length), 10); // in order to show css animation initially
+    } else {
+        getNodeList(arrayOfElements);
+        bar = document.querySelector('.js-progress-bar');
+        parts = setProgressBarParts(length);
+    }
+};
